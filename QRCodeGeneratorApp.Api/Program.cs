ad263ef0b253c5480e69153ee72253362420fdbe
Alexander Klassanov <klassanov@live.com>
2025-09-18 20:27:36 +0300

    Web API added to the solution

---
+++ QRCodeGeneratorApp.Api/Program.cs
@@ -0,0 +1,79 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi(options =>
+{
+    //Customize the generated Open Api document 
+    options.AddDocumentTransformer((document, context, canellationToken) =>
+    {
+        document.Info.Title = "Customized Open Api Title";
+        document.Info.Contact = new OpenApiContact
+        {
+            Name = "Alexander Klassanov",
+            Email = "test@test.com",
+            Url = new Uri("https://test.com")
+        };
+        document.Info.License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        };
+        return Task.CompletedTask;
+    });
+});
+
+
+
+var app = builder.Build();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
