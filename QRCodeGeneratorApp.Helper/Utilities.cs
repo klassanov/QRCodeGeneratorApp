aeca02553ff133bb445882b8bc01a9a8d85b7499
Alexander Klassanov <klassanov@live.com>
2025-12-26 16:30:46 +0200

    Added a Helper Project to publish to nuget feed

---
+++ QRCodeGeneratorApp.Helper/Utilities.cs
@@ -0,0 +1,10 @@
+﻿namespace QRCodeGeneratorApp.Helper
+{
+    public static class Utilities
+    {
+        public static int GenerateRandomNumber(int min = 0, int max = 1)
+        {
+            return Random.Shared.Next(min, max);
+        }
+    }
+}
